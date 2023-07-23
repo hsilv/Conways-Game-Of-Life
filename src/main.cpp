98e34472ebcf0da597d2f133790527beac64652a
hsilv <hm.sebastiansilva@gmail.com>
2023-07-23 01:55:40 -0600

    Game running

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,147 @@
 #include "framebuffer.h"
 #include "render.h"
 
-// Dimensiones de la ventana
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+const int CELL_SIZE = 5; // Tamaño de cada célula en píxeles
+
+// Dimensiones del grid del juego
+const int GRID_WIDTH = SCREEN_WIDTH / CELL_SIZE;
+const int GRID_HEIGHT = SCREEN_HEIGHT / CELL_SIZE;
+
+// Función para obtener el índice de una célula en el grid
+// Función para obtener el índice de una célula en el grid
+int getIndex(int x, int y)
+{
+    return y * GRID_WIDTH + x;
+}
+
+// Función para obtener el estado de una célula (viva o muerta) en una posición del grid
+bool getCellState(const std::vector<bool>& grid, int x, int y)
+{
+    int index = getIndex(x, y);
+    if (index >= 0 && index < grid.size())
+    {
+        return grid[index];
+    }
+    return false;
+}
+
+// Función para establecer el estado de una célula en una posición del grid
+void setCellState(std::vector<bool>& grid, int x, int y, bool state)
+{
+    int index = getIndex(x, y);
+    if (index >= 0 && index < grid.size())
+    {
+        grid[index] = state;
+    }
+}
+
+// Función para contar la cantidad de células vecinas vivas de una célula en una posición del grid
+int countAliveNeighbors(const std::vector<bool>& grid, int x, int y)
+{
+    int count = 0;
+    for (int i = -1; i <= 1; i++)
+    {
+        for (int j = -1; j <= 1; j++)
+        {
+            if (i == 0 && j == 0)
+                continue; // La célula en sí misma no cuenta como vecina
+            if (getCellState(grid, x + i, y + j))
+                count++;
+        }
+    }
+    return count;
+}
+
+// Función para actualizar el estado del grid según las reglas del "Game of Life"
+void updateGrid(std::vector<bool>& grid)
+{
+    std::vector<bool> newGrid = grid; // Creamos una copia del grid actual para almacenar el próximo estado
+
+    for (int y = 0; y < GRID_HEIGHT; y++)
+    {
+        for (int x = 0; x < GRID_WIDTH; x++)
+        {
+            int neighbors = countAliveNeighbors(grid, x, y);
+            bool currentState = getCellState(grid, x, y);
+            bool nextState = false;
+
+            // Aplicar las reglas del "Game of Life"
+            if (currentState)
+            {
+                if (neighbors < 2 || neighbors > 3)
+                {
+                    nextState = false; // Muere por falta de vecinos o por sobrepoblación
+                }
+                else
+                {
+                    nextState = true; // Sobrevive si tiene 2 o 3 vecinos vivos
+                }
+            }
+            else
+            {
+                if (neighbors == 3)
+                {
+                    nextState = true; // Revive si tiene exactamente 3 vecinos vivos
+                }
+            }
+
+            newGrid[getIndex(x, y)] = nextState; // Actualizamos el estado de la célula en la nueva copia
+        }
+    }
+
+    grid = newGrid; // Copiamos el grid actualizado de vuelta al grid original
+}
+
+// Función para establecer un patrón inicial de "Glider Gun" en el grid
+void setGosperGliderGunPattern(std::vector<bool>& grid, int x, int y)
+{
+    std::vector<std::string> gliderGunPattern = {
+        "........................O",
+        "......................O.O",
+        "............OO......OO............OO",
+        "...........O...O....OO............OO",
+        "OO........O.....O...OO",
+        "OO........O...O.OO....O.O",
+        "..........O.....O.......O",
+        "...........O...O",
+        "............OO"
+    };
+
+    for (int yy = 0; yy < gliderGunPattern.size(); yy++)
+    {
+        for (int xx = 0; xx < gliderGunPattern[yy].size(); xx++)
+        {
+            if (gliderGunPattern[yy][xx] == 'O')
+            {
+                setCellState(grid, x + xx, y + yy, true);
+            }
+        }
+    }
+}
+
+void setPufferTypeBreederPattern(std::vector<bool>& grid, int x, int y)
+{
+    std::vector<std::string> breederPattern = {
+        "OOO...O..O..O...OOO",
+        "O..O..O..O..O..O..O",
+        ".O.O..O..O..O..O.O.",
+        "O...O..O..O..O...O",
+        "..................."
+    };
+
+    for (int yy = 0; yy < breederPattern.size(); yy++)
+    {
+        for (int xx = 0; xx < breederPattern[yy].size(); xx++)
+        {
+            if (breederPattern[yy][xx] == 'O')
+            {
+                setCellState(grid, x + xx, y + yy, true);
+            }
+        }
+    }
+}
 
 int main(int argc, char *args[])
 {
@@ -21,7 +159,7 @@ int main(int argc, char *args[])
 
   // Crear la ventana
   SDL_Window *window = SDL_CreateWindow(
-      "Hello, SDL!",           // Título de la ventana
+      "Conway's Game Of Life",          // Título de la ventana
       SDL_WINDOWPOS_UNDEFINED, // Posición x de la ventana (centro)
       SDL_WINDOWPOS_UNDEFINED, // Posición y de la ventana (centro)
       SCREEN_WIDTH,            // Ancho de la ventana
@@ -50,6 +188,17 @@ int main(int argc, char *args[])
   framebuffer.resize(framebufferWidth * framebufferHeight);
   clear(); // Llenar el framebuffer con el color de fondo
 
+  // Centrar el grid de células en la pantalla
+  int gridOffsetX = (SCREEN_WIDTH - GRID_WIDTH * CELL_SIZE) / 2;
+  int gridOffsetY = (SCREEN_HEIGHT - GRID_HEIGHT * CELL_SIZE) / 2;
+
+  // Crear el grid de células del "Game of Life"
+  // Crear el grid del "Game of Life" y establecer los patrones iniciales
+    std::vector<bool> grid(GRID_WIDTH * GRID_HEIGHT, false);
+    setGosperGliderGunPattern(grid, GRID_WIDTH / 4, GRID_HEIGHT / 4);
+    setGosperGliderGunPattern(grid, GRID_WIDTH * 3 / 4 - 36, GRID_HEIGHT / 4);
+    setPufferTypeBreederPattern(grid, GRID_WIDTH / 2 - 5, GRID_HEIGHT * 3 / 4 - 2);
+
   bool running = true;
   SDL_Event event;
 
@@ -63,13 +212,39 @@ int main(int argc, char *args[])
       }
     }
 
-    setCurrentColor(Color(255, 0, 0)); // Color rojo
-    point(Vertex2(100, 100), framebufferWidth, framebufferHeight);
-    point(Vertex2(200, 200), framebufferWidth, framebufferHeight);
-    setCurrentColor(Color(0, 255, 0)); // Color verde
-    point(Vertex2(300, 300), framebufferWidth, framebufferHeight);
-    setCurrentColor(Color(0, 0, 255)); // Color azul
-    point(Vertex2(400, 400), framebufferWidth, framebufferHeight);
+    // Actualizar el grid según las reglas del "Game of Life"
+    updateGrid(grid);
+
+    // Renderizar el grid en el framebuffer
+    clear(); // Limpiar el framebuffer
+
+    for (int y = 0; y < GRID_HEIGHT; y++)
+    {
+      for (int x = 0; x < GRID_WIDTH; x++)
+      {
+        if (getCellState(grid, x, y))
+        {
+          // Si la célula está viva, dibujarla de color blanco
+          setCurrentColor(Color(255, 255, 255));
+        }
+        else
+        {
+          // Si la célula está muerta, dibujarla de color negro
+          setCurrentColor(Color(0, 0, 0));
+        }
+
+        // Dibujar la célula en el framebuffer
+        int pixelX = x * CELL_SIZE;
+        int pixelY = y * CELL_SIZE;
+        for (int dy = 0; dy < CELL_SIZE; dy++)
+        {
+          for (int dx = 0; dx < CELL_SIZE; dx++)
+          {
+            point(Vertex2(pixelX + dx, pixelY + dy), framebufferWidth, framebufferHeight);
+          }
+        }
+      }
+    }
 
     // Call our render function
     render(renderer);
@@ -78,7 +253,7 @@ int main(int argc, char *args[])
     SDL_RenderPresent(renderer);
 
     // Delay to limit the frame rate
-    SDL_Delay(1000 / 60);
+    SDL_Delay(1000 / 144); // 10 frames per second
   }
 
   // Liberar recursos
--- src/render.cpp
+++ src/render.cpp
@@ -37,20 +37,6 @@ void renderBuffer(SDL_Renderer* renderer) {
 }
 
 void render(SDL_Renderer* renderer) {
-    // Clear the framebuffer
-    clear();
-
-    // Update animation variables
-    angle += angleStep;
-    centerX = static_cast<int>(SCREEN_WIDTH / 2 + radius * std::cos(angle));
-    centerY = static_cast<int>(SCREEN_HEIGHT / 2 + radius * std::sin(angle));
-    currentColor = Color(static_cast<uint8_t>((std::sin(angle) + 1.0) * 127.0),
-                         static_cast<uint8_t>((std::cos(angle) + 1.0) * 127.0),
-                         static_cast<uint8_t>((std::sin(angle + M_PI) + 1.0) * 127.0));
-
-    // Draw the star with the updated center and radius
-    drawStar(centerX, centerY, radius, renderer);
-
     // Render the framebuffer to the screen
     renderBuffer(renderer);
 }
